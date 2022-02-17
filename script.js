011b517c94d2eef55eb63656f2bbc84346323538
Javier <javiaragoneses1@gmail.com>
2022-02-17 22:10:40 +0100

    Update

--- script.js
+++ script.js
@@ -32,24 +32,31 @@ function main() {
 
 
 function leerTree(elemento) {
-    var newChilds = elemento.children;
-    console.log("Hijos de " + elemento.nodeName);
-    for (var i = 0; i < newChilds.length; i++) {
-        if (newChilds[i].children.length > 0) {
-            console.log("Defino al padre AQUI " + newChilds[i].nodeName);
-            mostrarResultado.appendChild(document.createTextNode("<xs:element name=\"" + newChilds[i].nodeName + "\">"));
-            checkPadres(newChilds[i], elemento);
-            //escribirElementosHijos(true);
-            mostrarResultado.innerHTML += "<br>";
-            leerTree(newChilds[i]);
+    if (elemento.children.length > 0) {
+        var newChilds = elemento.children;
+        console.log("Hijos de " + elemento.nodeName);
+        for (var i = 0; i < newChilds.length; i++) {
+            if (newChilds[i].children.length > 0) {
+                console.log("Defino al padre AQUI " + newChilds[i].nodeName);
+                mostrarResultado.appendChild(document.createTextNode("<xs:element name=\"" + newChilds[i].nodeName + "\">"));
+                checkPadres(newChilds[i], elemento);
+                mostrarResultado.innerHTML += "<br>";
+                leerTree(newChilds[i]);
 
+            }
+            else {
+                console.log("Defino a un hijo de " + elemento.nodeName + " aqui " + newChilds[i].nodeName);
+                mostrarResultado.innerHTML += "<br>";
+                mostrarResultado.appendChild(document.createTextNode("--------------<xs:element name=\"" + newChilds[i].nodeName + "\" type=\"xs:string\"/>"));
+                mostrarResultado.innerHTML += "<br>";
+            }
         }
-        else {
-            console.log("Defino a un hijo de " + elemento.nodeName + " aqui " + newChilds[i].nodeName);
-            mostrarResultado.innerHTML += "<br>";
-            mostrarResultado.appendChild(document.createTextNode("--------------<xs:element name=\"" + newChilds[i].nodeName + "\" type=\"xs:string\"/>"));
-            mostrarResultado.innerHTML += "<br>";
-        }
+    }
+    else {
+        console.log("entro aki con " + elemento.nodeName);
+        mostrarResultado.innerHTML += "<br>";
+        mostrarResultado.appendChild(document.createTextNode("--------------<xs:element name=\"" + elemento.nodeName + "\" type=\"xs:string\"/>"));
+        mostrarResultado.innerHTML += "<br>";
     }
 
 }
@@ -61,26 +68,92 @@ function leerTree(elemento) {
 function checkPadres(newChilds, elemento) {
     const array_padres = elemento.getElementsByTagName(newChilds.nodeName);
     var num = array_padres.length;
-    if (num > 1) {
-     
 
-        
-        for (var x=0;x<newChilds.children.length;x++){
-            checkElemento(array_padres,newChilds.children[x]);
+
+    if (num > 1) {
+
+        var array = [];
+
+        for (var x = 0; x < newChilds.children.length; x++) {
+            checkElemento(array_padres, newChilds.children[x], array);
         }
+
+        console.log("------- Array resultado ------");
+        array.forEach(element => console.log(element));
+        console.log("------- Array resultado ------");
+
+        ponerNombres(array_padres, array);
+
 
 
         //leerTree(array_padres[0]);
-       // arrayTempRepetidos.push(newChilds.nodeName);
+        // arrayTempRepetidos.push(newChilds.nodeName);
     }
 }
 
-function checkElemento(array_padres,elementoC){
-    for (var i=0;i<array_padres.length;i++){
-        for (var j=0;j<array_padres[i].children.length;j++){
-            if (array_padres[i].children[j].nodeName==elementoC.nodeName){
-               console.log("aki: "+elementoC.nodeName);
+function checkElemento(array_padres, elementoC, array) {
+    var contador = 0;
+    for (var i = 0; i < array_padres.length; i++) {
+        for (var j = 0; j < array_padres[i].children.length; j++) {
+            if (array_padres[i].children[j].nodeName == elementoC.nodeName) {
+                contador++;
             }
         }
     }
+    if (contador == array_padres.length) {
+        array.push(elementoC.nodeName);
+    }
+    else {
+        console.log("no hay secuencia de " + elementoC.nodeName);
+    }
 }
+
+function ponerNombres(array_padres, array) {
+    console.log("----- Final ----");
+    for (var x = 0; x < array.length; x++) {
+        for (var y = 0; y < array_padres.length; y++) {
+            var m = array_padres[y].getElementsByTagName(array[x])[0];
+            if (x==0){
+                checkAr(array,m);
+            }
+            if (y == 0) {
+                console.log("Elemento repetido: " + array[x]);
+            }
+
+            checkAb(array, m);
+        }
+    }
+    console.log("----- Final ----");
+}
+
+function checkAb(array, m) {
+    var d = m;
+    while (true) {
+        d = d.nextElementSibling;
+        if (d == null) {
+            break;
+        }
+        else if (array.includes(d.nodeName)) {
+            break;
+        }
+        console.log("Elemento no repetido (ocurrencia 0 o X): " + d.nodeName)
+
+    }
+}
+
+function checkAr(array, m) {
+    var d = m;
+    while (true) {
+        d = d.previousElementSibling;
+        if (d == null) {
+            break;
+        }
+        else if (array.includes(d.nodeName)) {
+            break;
+        }
+        console.log("Elemento no repetido (ocurrencia 0 o X): " + d.nodeName)
+
+    }
+}
+
+
